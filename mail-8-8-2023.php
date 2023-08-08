ba0a9d88d6bc1b09fb495dbe616e4b0880d4514b
Samihan <74051958+sivasamih@users.noreply.github.com>
2023-08-08 17:13:27 +0530

    SU

--- mail-8-8-2023.php
+++ mail-8-8-2023.php
@@ -15,8 +15,7 @@
         $mail_to = "sales@tourni-s.com";  //sales@tourni-s.com
         
         # Sender Data
-        $subjectMaster ="Enquiry From Tourni-S Website";
-        $subject=str_replace(array("\r","\n"),array(" "," ") , strip_tags(trim($_POST["subject"])));
+        $subject ="Enquiry From Tourni-S Website";
         $name = str_replace(array("\r","\n"),array(" "," ") , strip_tags(trim($_POST["name"])));
         $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
         // $phone = trim($_POST["phone"]);
@@ -34,7 +33,6 @@
             exit;
         }
 
-        /*
         $ip = $_SERVER['REMOTE_ADDR'];
         // $response=file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=".$secretKey."&response=".$captcha."&remoteip=".$ip);
         // $responseKeys = json_decode($response,true);
@@ -62,76 +60,11 @@
             echo '<p class="alert alert-warning">Oops! Something went wrong, we couldnt send your message.</p>';
 
         }
-        */
-
-        
-
-        $spamList=['adrii.arag@hotmail.com','jessica_guevara6871@yahoo.com' ,'RobertDof'];
-
-        if (stripos(json_encode($spamList),$email) !== false || stripos(json_encode($spamList),$name) !== false  || $subjectMaster!==$subject) {
-            // echo "found mystring";
-            # Set a 500 (internal server error) response code.
-            http_response_code(500);
-            echo '<p class="alert alert-warning">Oops! Spam activity found, we couldnt send your message.</p>';
-
-        }else{
-
-            $data = array(
-                "email" => $email,
-                "name" => $name,
-                "subject" => $subject ,
-                "message" => "Message:\n$message\n",
-            );
-    
-            $reqData = array(
-                'WebsiteData' => array(
-                    "SourceID" => 5,
-                    "TypeID" => 1,
-                    "isChecked" => false,
-                    "JsonData" =>  json_encode($data)
-                )
-            );
-    
-        $curl = curl_init();
-        $APIURL1 = "http://203.122.12.35:8082/api/WebSite/Add_Update_WebsiteData";
-        curl_setopt_array($curl, [
-            CURLOPT_URL => $APIURL1,
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => "",
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 30,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            // CURLOPT_CUSTOMREQUEST => "GET",
-            CURLOPT_CUSTOMREQUEST => "POST",
-            CURLOPT_POSTFIELDS => json_encode($reqData),
-            CURLOPT_HTTPHEADER => [
-                'Content-Type:application/json'
-            ],
-        ]);
-        $response = curl_exec($curl);
-        $err = curl_error($curl);
-        curl_close($curl);
-        if ($err) {
-            //echo "true";
-            http_response_code(200);
-            echo '<p class="alert alert-success">Thank You! Your message has been sent.</p>';
-            $url = 'https://tourni-s.com/';
-            echo '<script>window.location = "' . $url . '";</script>';
-        } else {
-            //echo "true";
-            http_response_code(200);
-            echo '<p class="alert alert-success">Thank You! Your message has been sent.</p>';
-            $url = 'https://tourni-s.com/';
-            echo '<script>window.location = "' . $url . '";</script>';
-        }
-
-        }
-
-       
-
 
     } else {
         # Not a POST request, set a 403 (forbidden) response code.
         http_response_code(403);
         echo '<p class="alert alert-warning">There was a problem with your submission, please try again.</p>';
     }
+
+?>
